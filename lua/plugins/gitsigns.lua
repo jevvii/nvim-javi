ec3e4dbd63eeb3ab08e9f60a41331fc82a618ee3
jevvii <jayveemarcelo09@gmail.com>
2026-04-27 11:20:28 +0800

    fix(gitsigns): resolve circular dependency/loading error

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,11 +1,39 @@
 return {
-	{ "tpope/vim-fugitive", },
-	{
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup()
-			vim.keymap.set("n", "<leader>pr", ":Gitsigns preview_hunk<CR>", {})
-			vim.keymap.set("n", "<leader>bl", ":Gitsigns toggle_current_line_blame<CR>", {})
-		end
-	}
+    "lewis6991/gitsigns.nvim",
+    opts = {
+        on_attach = function(bufnr)
+            local gs = package.loaded.gitsigns
+
+            local function map(mode, l, r, opts)
+                opts = opts or {}
+                opts.buffer = bufnr
+                vim.keymap.set(mode, l, r, opts)
+            end
+
+            -- Navigation
+            map("n", "]c", function()
+                if vim.wo.diff then
+                    return "]c"
+                end
+                vim.schedule(function()
+                    gs.next_hunk()
+                end)
+                return "<Ignore>"
+            end, { expr = true })
+
+            map("n", "[c", function()
+                if vim.wo.diff then
+                    return "[c"
+                end
+                vim.schedule(function()
+                    gs.prev_hunk()
+                end)
+                return "<Ignore>"
+            end, { expr = true })
+
+            -- Actions
+            map("n", "<leader>pr", gs.preview_hunk)
+            map("n", "<leader>bl", gs.toggle_current_line_blame)
+        end,
+    },
 }
